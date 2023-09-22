f2b075a30973e877aca7c42c84adf6b997d4081f
ethannieskes <ethannieskes@gmail.com>
2023-09-21 22:31:21 -0500

    Got our first model and a semi functional login

--- UWM_Clubs_and_Events/views.py
+++ UWM_Clubs_and_Events/views.py
@@ -4,8 +4,8 @@ from .models import User
 
 class login(View):
     def get(self, request):
-        user = User.objects.create(email="test@uwm.edu", password="pass", name="Foo Bar")
-        user.save()
+        # user = User.objects.create(email="test@uwm.edu", password="pass", name="Foo Bar")
+        # user.save()
         return render(request, 'login.html', {"name": "login"})
 
     def post(self, request):
@@ -14,22 +14,23 @@ class login(View):
         try:
             email = request.POST['username']
             password = request.POST['password']
-            user = User.objects.get(email=request.POST['username'])
+            user = User.objects.get(email=(request.POST['username']))
             badPassword = (user.password != request.POST['password'])
         except:
             noSuchUser = True
+
         if email == '' and password == '':
             return render(request, "login.html", {"message": "Nothing entered"})
-        elif user == '':
-            return render(request, "login.html", {})
+        elif email == '':
+            return render(request, "login.html", {"message": "Empty email"})
         elif password == '':
-            return render(request, "login.html", {})
+            return render(request, "login.html", {"message": "Nothing empty password"})
         elif noSuchUser:
-            return render(request, "login.html", {})
+            return render(request, "login.html", {"message": "No user in the system"})
         elif badPassword:
-            return render(request, "login.html", {})
+            return render(request, "login.html", {"message": "No user with this password"})
         else:
-            return redirect("/homepage/")
+            return redirect("homepage.html")
 
 class Homepage(View):
     def get(self, request):
