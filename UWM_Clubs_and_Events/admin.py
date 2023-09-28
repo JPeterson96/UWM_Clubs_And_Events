5c2946c5ddca7d05082a7effdfd930b8c1181508
ethannieskes <ethannieskes@gmail.com>
2023-09-28 10:49:52 -0500

    I believe this iteration should be sufficient, this commit didn't register in admin for some reason.  I could add migration file but i am hesitant to do that

--- UWM_Clubs_and_Events/admin.py
+++ UWM_Clubs_and_Events/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Event, UserInterest, EventTag, Interest
 
 admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Interest)
+admin.site.register(UserInterest)
+admin.site.register(EventTag)
--- UWM_Clubs_and_Events/models.py
+++ UWM_Clubs_and_Events/models.py
@@ -10,27 +10,29 @@ class User(models.Model):
     email = models.EmailField(max_length=30, unique=True)  # add email validator?
     password = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=30)
+    role = models.PositiveSmallIntegerField(choices=((0, "Student"), (1, "Organization")))
 
     def __str__(self):
         return self.name
 
 
-# Org/Club:  name-str, point_of_contact-str/email, membersCount-int, description-str, staff-User(staff to anchor), majors-?
-class Organizations(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-    point_of_contact = models.ForeignKey(User, on_delete=models.CASCADE, to_field='email')
-    membersCount = models.IntegerField()
-    description = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
+# # Org/Club:  name-str, point_of_contact-str/email, membersCount-int, description-str, staff-User(staff to anchor), majors-?
+# class Organization(models.Model):
+#     name = models.CharField(max_length=20, unique=True)
+#     point_of_contact = models.ForeignKey(User, on_delete=models.CASCADE, to_field='email')
+#     membersCount = models.IntegerField()
+#     description = models.CharField(max_length=200)
+#
+#     def __str__(self):
+#         return self.name
 
 
 # Events:  name-str, organization-str, location-str, date-str/Date/Time, description-str, type-str, views-int
-class Events(models.Model):
-    name = models.CharField(max_length=30)
+class Event(models.Model):
+    name = models.CharField(max_length=30, unique=True)
     organization = models.CharField(max_length=30)  # foreign key this?
     location = models.CharField(max_length=30)
+    #this uses a YYYY-MM-DD for the date, and HH:MM:SS for the time
     time = models.DateTimeField()  # look into these 2 fields some more
     description = models.TextField()
 
@@ -39,24 +41,24 @@ class Events(models.Model):
         return self.name + "/" + self.organization
 
 
-class MembersIn(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='email')
-    name = models.ForeignKey(Organizations, on_delete=models.CASCADE, to_field='name')
+# class MembersIn(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='email')
+#     name = models.ForeignKey(Organizations, on_delete=models.CASCADE, to_field='name')
+#
+#     def __str__(self):
+#         return self.user + "/" + self.name
+#
+#
+# class Majors(models.Model):
+#     # consider using choices here for these?
+#     name = models.CharField(max_length=20, unique=True)
+#     department = models.CharField(max_length=20)
+#
+#     def __str__(self):
+#         return self.name + "/" + self.department
 
-    def __str__(self):
-        return self.user + "/" + self.name
 
-
-class Majors(models.Model):
-    # consider using choices here for these?
-    name = models.CharField(max_length=20, unique=True)
-    department = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name + "/" + self.department
-
-
-class Interests(models.Model):
+class Interest(models.Model):
     tag = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
@@ -64,9 +66,17 @@ class Interests(models.Model):
 
 
 # check how cascade actually works
-class UserInterests(models.Model):
+class UserInterest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='email')
-    type = models.ForeignKey(Interests, on_delete=models.CASCADE, to_field='tag')
+    type = models.ForeignKey(Interest, on_delete=models.CASCADE, to_field='tag')
 
     def __str__(self):
-        return self.user.email + "/" + self.type
+        return self.user.email + "/" + self.type.tag
+
+
+class EventTag(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, to_field='name')
+    interest = models.ForeignKey(Interest, on_delete=models.CASCADE, to_field='tag')
+
+    def __str__(self):
+        return self.event.name + "/" + self.interest.tag
