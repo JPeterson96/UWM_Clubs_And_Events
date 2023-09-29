97a9c6ba2e462d56955651f4a5bf336c4e48e421
14144 <kravtso5@uwm.edu>
2023-09-29 10:51:50 -0500

    Acceptence tests for createUser

--- Tests/UnitTests/createUserTest.py
+++ Tests/UnitTests/createUserTest.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-class test_createUser(TestCase):
+class TestCreateUser(TestCase):
 
     def test_createUser(self):
         self.made = createUser(self, name = "Ilya", email = "kravtso5@uwm.edu", password = "123")
@@ -10,7 +10,7 @@ class test_createUser(TestCase):
         self.assertEqual(self.user.name , "Ilya")
         self.assertEqual(self.user.password , "123")
 
-class test_invalid_input(TestCase):
+class TestInvalidInput(TestCase):
 
     def test_emptyName(self):
         self.made = test_createUser(self, name = "", email = "kravtso5@uwm.edu", password = "123")
