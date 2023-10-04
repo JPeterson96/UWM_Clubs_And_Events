f0c5d3933d75ec2841eeb98541b5d60b041a2672
14144 <kravtso5@uwm.edu>
2023-10-04 12:48:42 -0500

    fixed user util functions

--- Tests/UnitTests/userUtilTest.py
+++ Tests/UnitTests/userUtilTest.py
@@ -17,34 +17,34 @@ class TestGetAllUsers(TestCase):
     def test_getAllUsers(self):
         self.user = User()
         self.user.save()
-        self.assertTrue(self.user in User_Util.get_all_users(self), "User should have been found in all users")
+        self.assertTrue(self.user in User_Util.get_all_users(), "User should have been found in all users")
 
 class TestGetUserByName(TestCase):
     def test_getUserByName(self):
         self.user = User(name = "Ilya")
         self.user.save()
-        self.assertTrue(self.user in User_Util.get_user_by_name(self, "Ilya"), "Users should have been found by name")
+        self.assertTrue(self.user in User_Util.get_user_by_name("Ilya"), "Users should have been found by name")
 
 class TestGetUserByNameInvalid(TestCase):
     def test_getUserByNameInvalid(self):
-        self.assertFalse(User_Util.get_user_by_name(self, "NonExistent").exists(), "No users should have been found")
+        self.assertFalse(User_Util.get_user_by_name("NonExistent").exists(), "No users should have been found")
 
 class TestGetUsersByMajor(TestCase):
     def test_getUsersByMajor(self):
         self.user = User(major = "CS")
         self.user.save()
-        self.assertTrue(self.user in User_Util.get_users_by_major(self, "CS"), "Users should have been found by major")
+        self.assertTrue(self.user in User_Util.get_users_by_major("CS"), "Users should have been found by major")
 
 class TestGetUsersByMajorInvalid(TestCase):
     def test_getUsersByMajorInvalid(self):
-        self.assertFalse(User_Util.get_users_by_major(self, "NonExistent").exists(), "No users should have been found")
+        self.assertFalse(User_Util.get_users_by_major("NonExistent").exists(), "No users should have been found")
 
 class TestGetUsersByInterest(TestCase):
     def test_getUsersByInterest(self):
         self.user = User(interests = "CS")
         self.user.save()
-        self.assertTrue(self.user in User_Util.get_users_by_interest(self, "CS"), "Users should have been found by interest")
+        self.assertTrue(self.user in User_Util.get_users_by_interest("CS"), "Users should have been found by interest")
 
 class TestGetUsersByInterestInvalid(TestCase):
     def test_getUsersByInterestInvalid(self):
-        self.assertFalse(User_Util.get_users_by_interest(self, "NonExistent").exists(), "No users should have been found")
+        self.assertFalse(User_Util.get_users_by_interest("NonExistent").exists(), "No users should have been found")
