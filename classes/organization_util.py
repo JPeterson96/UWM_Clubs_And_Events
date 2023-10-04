5cf73d815fc7177ddebfe60ba58bef87dd160c35
14144 <kravtso5@uwm.edu>
2023-10-04 13:56:19 -0500

    made orgUtil tests

--- classes/organization_util.py
+++ classes/organization_util.py
@@ -12,35 +12,36 @@ class Organization_Util():
         try:
             org = Organization(name=name, point_of_contact=point_of_contact, members=members, description=description, role=role, interests=interests)
             org.save()
-            return org
-        except:
-            return None
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-    def get_org(self, name):
+    def get_org(name):
         try:
             return Organization.objects.get(name=name)
         except:
             return None
         
-    def get_all_orgs(self):
+    def get_all_orgs():
         try:
             return Organization.objects.all()
         except:
             return None
         
-    def get_org_by_interest(self, interests):
+    def get_org_by_interest(interests):
         try:
             return Organization.objects.filter(interests=interests)
         except:
             return None
         
-    def get_org_by_major(self, major):
+    def get_org_by_major(major):
         try:
             return Organization.objects.filter(major=major)
         except:
             return None
         
-    def get_members(self, name):
+    def get_members(name):
         try:
             return Organization.objects.get(name=name).members
         except:
