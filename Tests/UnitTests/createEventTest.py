8f1cc95b14042284cb9d1891c1386168377e2f33
Jamie <jamie215215@hotmail.com>
2023-10-05 12:14:37 -0500

    fixed classes and tests folders

--- Tests/UnitTests/createEventTest.py
+++ Tests/UnitTests/createEventTest.py
@@ -1,54 +1,49 @@
 from django.test import TestCase
+from classes.event_util import Event_Util as event_util
+import datetime
 
-class test_createEvent(TestCase):
+class TestCreateEvent(TestCase):
 
     def test_createEvent(self):
-        self.made = createEvent(self, name = "Club meeting", org = "CS Smart Club", location = "location", date = "date", 
-                                time = "time", description = "description", background_image = None, type = "type")
-        self.assertEqual(self.made, True)
+        self.time = datetime.datetime(year=2020, month=1, day=1, hour=0, minute=0, second=0, tzinfo = datetime.timezone.utc)
+        self.made = event_util.create_event("Club meeting", "CS Smart Club", "location", 
+                                            self.time , "description")
+        self.assertEqual(self.made, True, "Event should have been created, returned false")
 
-        self.event = getEvent(self, "Club meeting") #create event id to search for events?
-        self.assertEqual(self.event.name, "Club meeting")
-        self.assertEqual(self.event.org, "Cs Smart Club")
-        self.assertEqual(self.event.location, "location")
-        self.assertEqual(self.event.date, "date")
-        self.assertEqual(self.event.time, "time")
-        self.assertEqual(self.event.description, "description")
-        self.assertEqual(self.event.type, "type")
+        self.event = event_util.get_org_events("CS Smart Club").first()
+        self.assertEqual(self.event.name, "Club meeting", "Name should be Club meeting")
+        self.assertEqual(self.event.organization, "CS Smart Club", "Organization should be CS Smart Club")
+        self.assertEqual(self.event.location, "location", "Location should be location")
+        self.assertEqual(self.event.time, self.time, "Time should be 2020-01-01 00:00:00")
+        self.assertEqual(self.event.description, "description", "Description should be description")
 
-class test_invalid_input(TestCase):
+class TestInvalidInput(TestCase):
+
+    def setUp(self):
+        self.time = datetime.datetime(year=2020, month=1, day=1, hour=0, minute=0, second=0, tzinfo = datetime.timezone.utc)
 
     def test_emptyName(self):
-        self.made = createEvent(self, name = "", org = "CS Smart Club", location = "location", date = "date", 
-                                time = "time", description = "description", background_image = None, type = "type")
-        self.assertEqual(self.made, False)
-
+        with self.assertRaises(TypeError, msg="Missing Name"):
+            self.test = event_util.create_event(org = "CS Smart Club", location = "location", 
+                                                time = self.time , description = "description")
+            
     def test_emptyOrg(self):
-        self.made = createEvent(self, name = "Club meeting", org = "", location = "location", date = "date", 
-                                time = "time", description = "description", background_image = None, type = "type")        
-        self.assertEqual(self.made, False)
+        with self.assertRaises(TypeError, msg="Missing Organization"):
+            self.test = event_util.create_event(name = "Club meeting", location = "location", 
+                                                time = self.time , description = "description")
     
     def test_emptyLocation(self):
-        self.made = createEvent(self, name = "Club meeting", org = "CS Smart Club", location = "", date = "date", 
-                                time = "time", description = "description", background_image = None, type = "type")
-        self.assertEqual(self.made, False)  
-
-    def test_emptyDate(self):
-        self.made = createEvent(self, name = "Club meeting", org = "CS Smart Club", location = "location", date = "", 
-                                time = "time", description = "description", background_image = None, type = "type")
-        self.assertEqual(self.made, False)
-
+        with self.assertRaises(TypeError, msg="Missing Location"):
+            self.test = event_util.create_event(name = "Club meeting", org  ="CS Smart Club", 
+                                                time = self.time , description = "description")
+    
     def test_emptyTime(self):
-        self.made = createEvent(self, name = "Club meeting", org = "CS Smart Club", location = "location", date = "date", 
-                                time = "", description = "description", background_image = None, type = "type")
-        self.assertEqual(self.made, False)
-
+        with self.assertRaises(TypeError, msg="Missing Time"):
+            self.test = event_util.create_event(name = "Club meeting", org = "CS Smart Club", location = "location", 
+                                                 description = "description")
+            
     def test_emptyDescription(self):
-        self.made = createEvent(self, name = "Club meeting", org = "CS Smart Club", location = "location", date = "date", 
-                                time = "time", description = "", background_image = None, type = "type")
-        self.assertEqual(self.made, False)
+        with self.assertRaises(TypeError, msg="Missing Description"):
+            self.test = event_util.create_event(name = "Club meeting", org = "CS Smart Club", location = "location", 
+                                                time = self.time)
 
-    def test_emptyType(self):
-        self.made = createEvent(self, name = "Club meeting", org = "CS Smart Club", location = "location", date = "date", 
-                                time = "time", description = "description", background_image = None, type = "")
-        self.assertEqual(self.made, False)
---
+++ Tests/UnitTests/eventUtilTest.py
@@ -0,0 +1,46 @@
+from django.test import TestCase
+from UWM_Clubs_and_Events.models import Event
+from classes.event_util import Event_Util
+import datetime
+
+#class TestGetEvent(TestCase):
+#
+#    def test_getEventValid(self):
+#        self.event = Event(id = 1)
+#        self.event.save()
+#        self.assertEqual(Event_Util.getEvent(1), self.event)
+
+#class TestGetEventInvalid(TestCase):
+#
+#    def test_getEventInvalid(self):
+#        self.assertEqual(Event_Util.get_event(1), None, "Event should not have been found")
+
+class TestgetAllEvents(TestCase):
+
+    def test_getAllEvents(self):
+        self.event = Event(time = datetime.datetime.now())
+        self.event.save()
+        self.assertTrue(self.event in Event_Util.get_all_events())
+
+class TestgetOrgEvents(TestCase):
+    def test_getOrgEvents(self):
+        self.event = Event(organization = "CS Smart Club", time = datetime.datetime(2020, 1, 1, 0, 0, 0, datetime.timezone.utc))
+        self.event.save()
+        self.assertTrue(self.event in Event_Util.get_org_events("CS Smart Club"))
+
+class TestgetOrgEventsInvalid(TestCase):
+     
+    def test_getOrgEventsInvalid(self):
+        self.assertFalse(Event_Util.get_org_events("NonExistent").exists(), "No orgs should have been found")
+
+class TestgetEventByName(TestCase):
+         
+    def test_getEventByName(self):
+        self.event = Event(name = "Club meeting", time = datetime.datetime(2020, 1, 1, 0, 0, 0, datetime.timezone.utc))
+        self.event.save()
+        self.assertTrue(self.event in Event_Util.get_event_by_name("Club meeting"))
+
+class TestgetEventByNameInvalid(TestCase):
+
+    def test_getEventByNameInvalid(self):
+        self.assertFalse(Event_Util.get_event_by_name("NonExistent").exists(), "No events should have been found")
