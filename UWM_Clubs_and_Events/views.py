5599485179c5f3668c55a0b0a5d520c84f31fd62
Jamie <jamie215215@hotmail.com>
2023-10-05 22:47:33 -0500

    login page functionality finished

--- UWM_Clubs_and_Events/views.py
+++ UWM_Clubs_and_Events/views.py
@@ -2,8 +2,7 @@ from django.views import View
 from django.shortcuts import render, redirect
 from .models import User, Major, Interest, UserInterest
 from classes import user_util
-
-
+from django.contrib.auth import authenticate, login as auth_login
 
 class login(View):
     def get(self, request):
@@ -13,23 +12,30 @@ class login(View):
         noSuchUser = False
         badPassword = False
         try:
-            email = request.POST['username']
-            password = request.POST['password']
-            user = User.objects.get(email=request.POST['username'])
-            badPassword = (user.password != request.POST['password'])
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = User.objects.get(email=request.POST.get('email'))
+            badPassword = (user.password != request.POST.get('password'))
         except:
             noSuchUser = True
         if email == '' and password == '':
-            return render(request, "login.html", {"message": "Nothing entered"})
+            return render(request, "login.html", {"error_message": "Nothing entered"})
         elif email == '':
-            return render(request, "login.html", {"message": "No email"})
+            return render(request, "login.html", {"error_message": "No email"})
         elif password == '':
-            return render(request, "login.html", {"message": "no password"})
+            return render(request, "login.html", {"error_message": "no password"})
+        
+        # user = authenticate(request, email=email, password=password)
+
+        # if user is not None:
+        #     auth_login(request, user)
+        #     return redirect("homepage")
         elif noSuchUser:
-            return render(request, "login.html", {"message": "no user in database"})
+            return render(request, "login.html", {"error_message": "no user in database"})
         elif badPassword:
-            return render(request, "login.html", {"message": "no user with this password"})
+            return render(request, "login.html", {"error_message": "no user with this password"})
         else:
+            # return render(request, "login.html", {"error_message": "no user with this password"})
             return redirect("homepage.html")
 
 
@@ -39,10 +45,7 @@ class Homepage(View):
 
 class CreateAccount(View):
 
-
     def get(self, request):
-
-
         search = request.GET.get('search-input', '')
         filtered_interests = Interest.objects.filter(tag__icontains=search)
         majors = Major.objects.all()
@@ -87,3 +90,4 @@ class CreateAccount(View):
             elif password == '':
                 return render(request, "createaccount.html", {"message": "no password"})
         return render(request, "login.html", {"message": "user account successfully created"})
+    
