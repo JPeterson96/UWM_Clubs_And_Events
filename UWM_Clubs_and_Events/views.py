bd849c654bb33593ca6910151b24ca7d209d6f34
OmarAbu <omar.aburmaileh2@gmail.com>
2023-11-13 23:10:08 -0600

    updated some minor fixes accross the different files

--- UWM_Clubs_and_Events/views.py
+++ UWM_Clubs_and_Events/views.py
@@ -290,15 +290,20 @@ class CreateOrganization(View):
 
         newOrg.save()
 
-        return render(request, "createorganization.html.html", {"success_message": "Organization Successfully created"})
+        return render(request, "createorganization.html", {"success_message": "Organization Successfully created"})
 
 
 class EditOrganization(View):
     def get(self, request):
         cur_user = user_util.User_Util.get_user(request.session['user'])
-        org = Organization.objects.get(user=cur_user)
+        if cur_user.role == 3:
+
+            org = Organization.objects.filter(point_of_contact=cur_user)
+            print()
+        else:
+            org = Organization.objects.filter(user=cur_user)
         pocs = User.objects.filter(role=3)
-        return render(request, 'editorganization.html', {'user': cur_user, 'organization': org, 'contacts': pocs})
+        return render(request, 'editorganization.html', {'user': cur_user, 'organizations': org, 'contacts': pocs})
 
     def post(self, request):
         cur_user = user_util.User_Util.get_user(request.session['user'])
