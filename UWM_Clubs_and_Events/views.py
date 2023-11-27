2499cd438824c1d8d38c20ac1ff43af568c3bebe
Jamie Peterson <jamie215215@hotmail.com>
2023-11-27 15:26:43 -0600

    added clear all filtering button

--- UWM_Clubs_and_Events/views.py
+++ UWM_Clubs_and_Events/views.py
@@ -103,14 +103,39 @@ class Homepage(View):
 
 class ClearFilters(View):
     def get(self, request):
-        del request.session['filters']
+        if 'filters' in request.session:
+            del request.session['filters']
+
+        current_user = user_util.User_Util.get_user(email=request.session['user'])
+        user_interests = StudentInterest.objects.filter(student__user=current_user)
+        interest_tags = [interest.type.tag for interest in user_interests]
 
         current_user = user_util.User_Util.get_user(email=request.session['user'])
         all_events = event_util.Event_Util.filter_events(
             user=current_user,
             sort_type=0,
             order=0,
-            by_date=0)
+            by_date=0,
+            interests=interest_tags)
+
+        paginator = Paginator(all_events, 5)
+        page_number = request.GET.get('page')
+        events = paginator.get_page(page_number)
+
+        return render(request, "homepage.html", {"Events": events, "user": current_user})
+    
+class ClearAllFilters(View):
+    def get(self, request):
+        if 'filters' in request.session:
+            del request.session['filters']
+
+        current_user = user_util.User_Util.get_user(email=request.session['user'])
+        all_events = event_util.Event_Util.filter_events(
+            user=current_user,
+            sort_type=0,
+            order=0,
+            by_date=0,
+            interests=None)
 
         paginator = Paginator(all_events, 5)
         page_number = request.GET.get('page')
